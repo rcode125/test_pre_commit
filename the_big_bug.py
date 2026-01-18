80cb345381ece28a5ecd2638776b074f0cdf608c
rcode125 <wht8465@abwesend.de>
2026-01-18 17:54:49 +0100

    Create the_big_bug.py

---
+++ the_big_bug.py
@@ -0,0 +1,12 @@
+import   os,sys
+import json
+from   datetime import  datetime
+import   math
+
+def   foo(   x:int ,y:int)->int:
+    if(x>  10):
+        print(   "Value is greater than 10"  )
+        return   x+y
+    else:
+        print("small")
+        return   x- y
